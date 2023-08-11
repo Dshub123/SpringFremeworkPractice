5956672e8c94f8ced0da50f10ef11d7481b8c095
Shubham Diddi <shubhamdiddi@gmail.com>
2023-08-11 15:02:16 +0530

    spring mvc

--- springmvcapp/src/main/java/springmvcapp/controller/HomeController.java
+++ springmvcapp/src/main/java/springmvcapp/controller/HomeController.java
@@ -1,15 +1,29 @@
 package springmvcapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("this is home url");
+		
+		model.addAttribute("name", "Shubham Diddi");  //model object wraped with attribute and value
+		
 		return "index";
 	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		System.out.println("this is sbout url");
+		return "about";
+	}
+	
+	
+	
 }
--- springmvcapp/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvcapp/src/main/webapp/WEB-INF/views/index.jsp
@@ -9,5 +9,12 @@
 <body>
 	<h1>Welcome to the Home Page</h1>
 	<p>this is spring mvc web application</p>
+	
+	<% //scriptlet tag
+		String name = (String) request.getAttribute("name"); //this came from home controller
+	
+	%>
+	
+	<h3>Name is <%=name%></h3> <!-- expression tag is used to show the dynamic value to the user -->
 </body>
 </html>
